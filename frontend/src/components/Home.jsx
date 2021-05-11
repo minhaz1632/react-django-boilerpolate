fc09523a78037ae9bb6af868018adce36e5e78e8
minhaz1632 <minhaz1632@gmail.com>
2021-05-11 23:20:40 +0600

    Hot export home compoenent

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { hot } from "react-hot-loader/root";
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <h1>This a component.</h1>
     </div>
   );
 }
+
+export default hot(Home);
